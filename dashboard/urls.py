41f8345092d69db180873aae1bb42550bad4fb0a
Aditya Prakash <aditya@localhost.localdomain>
2015-05-25 15:23:51 +0530

    Added urls for ip handler

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,11 +1,7 @@
 from django.conf.urls import url
 
+from . import views
 urlpatterns = [
-    url(),
-]
-urlpatterns = [
-	url(r'^$', IndexView.as_view(), name = 'index'),
-	url(r'^(?P<pk>[0-9]+)/$', DetailView.as_view(), name='details'),
-	url(r'^(?P<pk>[0-9]+)/result/$',ResultView.as_view(), name='result'),
-	url(r'^(?P<ques_id>[0-9]+)/vote/$', vote, name='vote'),
+    url(r'^add_ip/$', views.add_ip, name='addIp'),
+    url(r'^delete_ip/$', views.delete_ip, name='deleteIp'),
 ]
