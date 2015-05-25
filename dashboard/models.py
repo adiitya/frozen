2af626812656f7b0c4d131baf63536da8037ec0c
Dhaval Kapil <me@dhavalkapil.com>
2015-05-25 11:57:58 +0530

    Added basic UserProfile model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+
+class UserProfile(models.Model):
+    # Links UserProfile to an actual User model
+    user = models.OneToOneField(User)
+
+    # The last access time of the user
+    last_access = models.DateTimeField('last access')
+
+    # Whether the user is alive or not
+    alive = models.BooleanField(default = False)
