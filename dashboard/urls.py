64de464f67038d203a0dd15aa5ccc2f47ec62299
Dhaval Kapil <me@dhavalkapil.com>
2015-05-25 13:43:22 +0530

    Added support for changing password by adding view for settings

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
   url(r'^login/$', views.user_login, name='login'),
+  url(r'^settings/$', views.user_settings, name='settings'),
   url(r'^logout/$', views.user_logout, name='logout'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -25,6 +25,23 @@ def user_login(request):
             error = "Invalid credentials"
     return render(request, 'dashboard/login.html', { 'error': error})
 
+def user_settings(request):
+    # Checking if user is logged in or not
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('dashboard:login'))
+
+    error = "";
+    if request.method == 'POST':
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        if password1 != password2:
+            error = 'Passwords do not match'
+        else:
+            request.user.set_password(password1)
+            request.user.save()
+            return HttpResponse('Password Successfully changed')
+    return render(request, 'dashboard/settings.html', { 'error': error})
+
 def user_logout(request):
     logout(request)
 
