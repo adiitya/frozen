d1e9bbfb5e110a3b5dfa903dea329a1cb15c57e4
Aditya Prakash <adiitya.adda@gmail.com>
2015-05-28 16:06:39 +0530

    Added middleware for modifying requests before serving

---
+++ frozen/middlewares.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from dashboard.models import IPs, UserIpMap, UserProfile
+from django.conf import settings
+from django.contrib.auth import authenticate, login, logout
+import re
+from dashboard import views
+
+
+class ValidateRequestMiddleware:
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        if not request.user.is_authenticated():
+            if request.META['PATH_INFO'] == '/login/' or 'admin' in request.META['PATH_INFO'] :
+                None
+            else:
+                return HttpResponseRedirect(reverse('dashboard:login'))
+        else:
+            None
