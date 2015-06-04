e5145de4084d87f4263ef9c05e11e78b91240e9e
Dhaval Kapil <me@dhavalkapil.com>
2015-06-04 12:36:50 +0530

    Shifted add and delete IP to ajax

---
+++ dashboard/static/dashboard/js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $('#add_ip_button').click(function() {
+        $.ajax({
+            type: "GET",
+            url: "/add_ip",
+            data: {
+                ip: $('#add_form [name="ip"]').val(),
+                polling_time: $('#add_form [name="polling_time"]').val()
+            },
+            success: function(data) {
+                if(data['error'])
+                    $('#add_response').html('<b>'+data['error']+'</b>');
+                else
+                    $('#add_response').html(data['success']);
+            }
+        })
+    });
+    $('#delete_ip_button').click(function() {
+        $.ajax({
+            type: "GET",
+            url: "/delete_ip",
+            data: {
+                ip: $('#delete_form [name="ip"]').val()
+            },
+            success: function(data) { 
+                if(data['error'])
+                    $('#delete_response').html('<b>'+data['error']+'</b>');
+                else
+                    $('#delete_response').html(data['success']);
+            }
+        })
+    });
+});
